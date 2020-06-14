c9289dd9e9c8acf58e29ded5ec616a8e660652b8
Javad <jd.mn98@yahoo.com>
2020-06-14 16:51:15 +0430

    Add project files.

---
+++ pgpConsole/PGPWorker.cs
@@ -0,0 +1,26 @@
+﻿using PgpCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pgpConsole
+{
+    public class PGPWorker
+    {
+        private static string GetLocalPath()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            return path;
+        }
+
+        public static string EncryptPGP(string filename , string publicFileName)
+        {
+            string path = $@"{GetLocalPath()}\{filename}.pgp";
+            using (PGP pgp = new PGP())
+            {
+                pgp.EncryptFile(filename, path , publicFileName, true, true);
+            }
+            return path;
+        }
+    }
+}
---
+++ pgpConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pgpConsole
+{
+    class Program
+    {
+
+        private static string GetCleanPath()
+        {
+            string certAddr = Console.ReadLine();
+            if (certAddr[0] == '"' && certAddr[^1] == '"')
+            {
+                certAddr = certAddr[1..^1];
+            }
+            return certAddr;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to PGP Console App");
+            Console.WriteLine("Enter Text File Address :");
+            string textFileAddress = GetCleanPath();
+            Console.WriteLine();
+            Console.WriteLine("Enter Public Key");
+            
+        }
+    }
+}
